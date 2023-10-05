1c388cd4cb65a0852407c9a30518401bd54ec571
Klinx-rdlr <rhodedannielreyes@gmail.com>
2023-10-05 11:15:55 +0800

    --FinalCode

--- Midterm.java
+++ Midterm.java
@@ -1,18 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package midterm;
-
 import java.util.Scanner;
 
 public class Midterm {
     public static Scanner getinput = new Scanner(System.in);
  
     public static void main(String[] args) {
-           Salary object = new Salary("OMANOIL", "S");
+           System.out.println("**********************************************");
+           Salary object1 = new Salary("OMANOIL", "S");
+           object1.printDetails();
+           object1.computeTotal();
+           object1.printTotal();
            Salary object2 = new Salary("SHELL", "T");
+           object2.printDetails();
+           object2.computeTotal();
+           object2.printTotal();
     }  
 }
 class Employee{
@@ -22,30 +22,41 @@ class Employee{
         this.company = company;
         this.category= category;
     }
+    
+    public void printDetails(){
+        System.out.println("COMPANY: " + this.company + "\t\t" + "CATEGORY: " + this.category);
+    }
 }
     class Salary extends Employee{
 
-    private String name;
-    private final double basic;
-    private final double overtime;
-    private final double total;
+    private  String name;
+    private  double basic;
+    private  double overtime;
+    private  double total;
     
     public Salary(String company, String category){
         super(company, category);
         if(company.equals("OMANOIL") && category.equals("S")){
             this.name = company;
             this.basic = 550.0;
-            this.overtime = basic * 0.05;
-            this.total = this.basic + this.overtime;
-            System.out.println("TOTAL SALARY " + this.name + " = " + this.total);
+            this.overtime = this.basic * 0.05;
         }else if(company.equals("SHELL") && category.equals("T")){
             this.name = company;
             this.basic = 350.0;
-            this.overtime = basic * 0.1;
-            this.total = this.basic + this.overtime;
+            this.overtime = this.basic * 0.1;
+        } else{
+           System.out.println("Invalid input, please try again");
+        }
+    }   
+      public void computeTotal(){
+          this.total = this.basic + this.overtime;
+      }
+    
+      public void printTotal(){
             System.out.println("TOTAL SALARY " + this.name + " = " + this.total);
-        } 
-    }
+            System.out.println("**********************************************");
+            
+        };
+      
 }
    
-
