95e8f57a7b6d9b51bbe268fb72477475b00b330c
Klinx-rdlr <rhodedannielreyes@gmail.com>
2023-10-05 11:37:51 +0800

    -added newChecks

--- Midterm.java
+++ Midterm.java
@@ -5,16 +5,17 @@ public class Midterm {
     public static Scanner getinput = new Scanner(System.in);
  
     public static void main(String[] args) {
+            //1st - Company, 2nd - Category, 3rd - Name;
            System.out.println("**********************************************");
-           Salary object1 = new Salary("OMANOIL", "S");
+           Salary object1 = new Salary("OMANOIL", "S", "Gian");
            object1.printDetails();
            object1.computeTotal();
            object1.printTotal();
-           Salary object2 = new Salary("SHELL", "T");
+           Salary object2 = new Salary("SHELL", "T", "Rhode");
            object2.printDetails();
            object2.computeTotal();
            object2.printTotal();
-           Salary object3 = new Salary("Test", "T");
+           Salary object3 = new Salary("Test", "T", "Test");
            object3.printDetails();
            object3.printTotal();
     }  
@@ -33,23 +34,24 @@ class Employee{
 }
     class Salary extends Employee{
 
-    private  String name;
-    private  double basic;
-    private  double overtime;
+    private  final String name;
+    private  final double basic;
+    private  final double overtime;
     private  double total;
     
-    public Salary(String company, String category){
+    public Salary(String company, String category, String name){
         super(company, category);
+        this.name = name;
         if(company.equals("OMANOIL") && category.equals("S")){
-            this.name = company;
             this.basic = 550.0;
             this.overtime = this.basic * 0.05;
         }else if(company.equals("SHELL") && category.equals("T")){
-            this.name = company;
             this.basic = 350.0;
             this.overtime = this.basic * 0.1;
         } else{
            System.out.println("Invalid input, please try again");
+           this.basic = 0;
+           this.overtime = 0;
         }
     }   
       public void computeTotal(){
@@ -57,6 +59,7 @@ class Employee{
       }
     
       public void printTotal(){
+            System.out.println("NAME: " + this.name);
             System.out.println("TOTAL SALARY " + this.name + " = " + this.total);
             System.out.println("**********************************************");
             
